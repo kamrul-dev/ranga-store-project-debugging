0bc211df93b68e57662d03a7612f4dca316c9482
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-10 00:12:02 +0600

    [Bug Fixed]: product price convert to parseInt

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
+    const price = parseInt(product.price);
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -18,8 +19,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <h2>Price: $ ${price}</h2>
+      <button onclick="addToCart(${product.id},${price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
